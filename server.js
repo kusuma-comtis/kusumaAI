7c3692fe26066561fec38d06b21eb9953b9298f0
Hanzy <sukawijaua@gmail.com>
2025-09-07 22:19:36 +0800

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
